f0ceb45f4c29702fa8d41ca7e19415a466b6b6e6
OskarDubas-dev <oohdee.dev@gmail.com>
2022-01-26 12:00:22 +0000

    classes added

---
+++ Source/ShooterPrototype/ShooterPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERPROTOTYPE_API AShooterPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
