0544a58325270e75c1694e7350fceb302221e1da
OskarDubas-dev <oohdee.dev@gmail.com>
2022-02-11 10:36:32 +0000

    Default mode changed to custom one, added game end fail condition if player die

--- Source/ShooterPrototype/KillEmAllGameMode.cpp
+++ Source/ShooterPrototype/KillEmAllGameMode.cpp
@@ -3,7 +3,17 @@
 
 #include "KillEmAllGameMode.h"
 
+#include "PlayerCharacter.h"
+
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
+
+	//check if Pawn killed is the player
+	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled);
+	if(PlayerController != nullptr)
+	{
+		PlayerController->GameHasEnded(nullptr, false);
+	}
+	
 }
